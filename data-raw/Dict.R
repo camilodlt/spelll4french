6f1e0ada24e4dcb860ba716b1f5ba9c4f8f2e9fa
Camilo De La Torre <dlt.camilo@gmail.com>
2021-04-09 11:08:31 +0200

    Functions to generate the dictionnary and the set of token characters

---
+++ data-raw/Dict.R
@@ -0,0 +1,28 @@
+## code to prepare `Dict` dataset goes here
+
+library(dplyr)
+
+x<-read.delim("lexique-grammalecte-fr-v7.0.txt",
+              header = T,sep="\t",skip=14,encoding = "UTF-8")
+
+# Normalize function
+
+normalize <- function(x)
+{
+  return((x- min(x)) /(max(x)-min(x)))
+}
+
+# Select useful columns
+x<-x%>% select(Flexion,Lemme,Notes,Total.occurrences)
+
+# Remove non unique words (keep the one with highest frequency)
+x<-x%>% arrange(desc(Total.occurrences))%>% distinct(Flexion,.keep_all=T)
+
+# Ensure frequency is at least 1. Augment frequency by one.
+x$Total.occurrences<- x$Total.occurrences+1
+
+# Normalize frequency count
+x$Total.occurrences<- normalize(x$Total.occurrences)
+
+dict<- x
+usethis::use_data(dict,internal = TRUE,overwrite = TRUE)
---
+++ data-raw/Letters.R
@@ -0,0 +1,23 @@
+## code to prepare `Letters` dataset goes here
+
+library(dplyr)
+library(tokenizers)
+
+x<-read.delim("lexique-grammalecte-fr-v7.0.txt",
+              header = T,sep="\t",skip=14,encoding = "UTF-8")
+
+# distinct words
+x<-x%>% distinct(Flexion)
+
+all_tokens<-x%>% filter(nchar(Flexion)>1)%>% .$Flexion%>%
+  tokenizers::tokenize_characters(.,strip_non_alphanum=TRUE)
+
+extended_tokens <- unique(unlist(all_tokens))
+
+tokens<- c("µ", "a", "á", "à", "â", "ä", "ã","æ", "b", "c",
+  "ç", "d", "e", "é", "è", "ê", "ë", "f", "g", "h", "i", "í",
+  "î", "ï", "j", "k", "l", "m", "n","o", "ó",
+  "ô", "ö","œ", "p", "q", "r","s", "t", "u", "ú", "ù",
+  "û", "ü", "v", "w", "x", "y", "ÿ", "z")
+
+usethis::use_data(extended_tokens,tokens,internal = TRUE,overwrite = TRUE)
