a329e3bc63e52e61f2d003d1d1a86447156e8a0f
Camilo De La Torre <dlt.camilo@gmail.com>
2021-04-10 08:55:13 +0200

    Check dictionary is now made with data.table joins for speed.

--- R/check_dictionary.R
+++ R/check_dictionary.R
@@ -5,12 +5,19 @@ proper_noun<- function(word){
 
 check_proper_dictionary<- function(word,dictionary=dict){
   proper= proper_noun(word)
-  if(proper %in% dict$Flexion) {return(TRUE)} else {return(FALSE)}
+  #if(proper %in% dict$Flexion) {return(TRUE)} else {return(FALSE)}
+  # With DT
+  subset<-test[Flexion==proper][['Flexion']]
+  if(!identical(subset, character(0))){return(TRUE)} else {return(FALSE)}
 }
 
 check_dictionary<- function(word,dictionary=dict){
 
-  if(word %in% dict$Flexion) {return(TRUE)} else {return(FALSE)}
+  #if(word %in% dict$Flexion) {return(TRUE)} else {return(FALSE)}
+
+  # With DT
+  subset<-test[Flexion==word][['Flexion']]
+  if(!identical(subset, character(0))){return(TRUE)} else {return(FALSE)}
 
 }
 
@@ -20,20 +27,27 @@ check_words<- function(word,dictionary= dict,...){
   proper_in_dict=check_proper_dictionary(word,dictionary)
 
 # Upper case needed
-  if(!word_in_dict& proper_in_dict){
+  if(!word_in_dict & proper_in_dict){
     return(stringr::str_to_title(word))
   }
 #
-  if(word_in_dict )
+  if(word_in_dict)
+    # independly if proper in dict or not, return the original word
   {return(word)}
 
 # decision
   if(!word_in_dict & !proper_in_dict){
     results = apply_depth(word = word,...)
     results=unique(unlist(results))
-    in_dict=purrr::map_lgl(results, check_dictionary, dictionary)
-    indices= which(in_dict)
-    extract = results[indices]
+    # DT
+    results= data.frame(Flexion=results)
+    setDT(results,key = 'Flexion')
+    subset<-dictionary[results,nomatch=0]
+      # Check emptyness
+    if(identical(subset, character(0))){return(word) else {subset[['Flexion']]}}
+    #in_dict=purrr::map_lgl(results, check_dictionary, dictionary)
+    #indices= which(in_dict)
+    #extract = results[indices]
     return(extract)
   }
 }
